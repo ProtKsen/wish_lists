5cbf7d99b3e646b2518b4f69e9ebd1b026ade344
ProtKsen <protcenko.kseniya@gmail.com>
2022-01-21 21:22:26 +0500

    add login

--- authentication/views.py
+++ authentication/views.py
@@ -1,6 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth import authenticate, login
 
 
 def authentication(request):
+    if request.method == "POST":
+        name = request.POST['name']
+        password = request.POST['password']
+        user = authenticate(request, username=name, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            print("Incorrect password")
     return render(request, 'authentication.html')
