d7ad156c2d408408eb9f1ec0bdbb7a18cde71222
ProtKsen <protcenko.kseniya@gmail.com>
2023-02-26 20:28:56 +0500

    chore: change project's structure

--- src/wish_lists/settings.py
+++ src/wish_lists/settings.py
@@ -22,7 +22,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-dotenv_file = os.path.join(BASE_DIR, ".env")
+ENV_DIR = Path(__file__).resolve().parent.parent.parent
+dotenv_file = os.path.join(ENV_DIR, ".env")
 if os.path.isfile(dotenv_file):
     dotenv.load_dotenv(dotenv_file)
 
