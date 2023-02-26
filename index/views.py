3adba23582ea76779ed80b693e9fd229e6480912
ProtKsen <protcenko.kseniya@gmail.com>
2023-02-26 13:44:31 +0500

    bugfix: linter's fixes

--- index/views.py
+++ index/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def home(request):
@@ -8,6 +8,7 @@ def home(request):
 
 def contacts(request):
     return render(request, 'contacts.html')
+
 
 def about(request):
     return render(request, 'about.html')
