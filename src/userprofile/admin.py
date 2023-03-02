7fbcef629ac86dfb7accd4217fde1b3fdd5c9789
ProtKsen <protcenko.kseniya@gmail.com>
2023-03-02 18:04:36 +0500

    bugfix: import pathes

--- src/userprofile/admin.py
+++ src/userprofile/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
 
-from models import models
+from . import models
 
 admin.site.register(models.Wish)
--- src/userprofile/urls.py
+++ src/userprofile/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from userprofile import views
+from . import views
 
 urlpatterns = [
     path('', views.userprofile, name='userprofile'),
