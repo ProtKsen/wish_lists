db05af563b9b1465225a528dfdaa724bcc9a059a
ProtKen <protcenko.kseniya@gmail.com>
2023-06-13 17:33:09 +0500

    tests: use pytest

---
+++ conftest.py
@@ -0,0 +1,14 @@
+import pytest
+from django.contrib.auth.models import User
+
+
+@pytest.fixture
+def create_user(django_db_setup, django_db_blocker):
+    with django_db_blocker.unblock():
+        return User.objects.create_user(username="TestUser", password="TestPassword")
+
+
+@pytest.fixture
+def login_user(django_db_setup, django_db_blocker, client, create_user):
+    client.login(username="TestUser", password="TestPassword")
+    return client
--- src/index/tests.py
+++ src/index/tests.py
@@ -1,3 +1,9 @@
-from django.test import TestCase
+from django.urls import reverse
+from pytest_django.asserts import assertTemplateUsed
 
-# Create your tests here.
+
+def test_home_get_request_successed(client):
+    url = reverse("home")
+    response = client.get(url)
+    assert response.status_code == 200
+    assertTemplateUsed(response, "home.html")
