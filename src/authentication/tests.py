8c81052b90e200dc303e077890fe787d4d64244a
ProtKen <protcenko.kseniya@gmail.com>
2023-06-14 16:59:48 +0500

    tests: add tests for logout

--- src/authentication/tests.py
+++ src/authentication/tests.py
@@ -1,7 +1,11 @@
 import pytest
-from django.contrib.auth.models import User
+from django.contrib import auth
 from django.urls import reverse
 from pytest_django.asserts import assertTemplateUsed
+
+"""
+Tests for login
+"""
 
 
 def test_authlogin_get_request_from_unauthorized_user_successed(client):
@@ -29,6 +33,8 @@ def test_authlogin_post_request_valid_user_data_successed(client, create_user):
     response = client.post(url, data=form_data, follow=True)
     assert response.status_code == 200
     assertTemplateUsed(response, "user_profile.html")
+    user = auth.get_user(client)
+    assert user.is_anonymous is False
 
 
 @pytest.mark.django_db
@@ -51,5 +57,28 @@ def test_authlogin_post_request_not_valid_password_show_error_message(client, cr
         "password": "WrongPassword",
     }
     response = client.post(url, data=form_data)
+    assert response.status_code == 200
+    assertTemplateUsed(response, "login.html")
+
+
+"""
+Tests for logout
+"""
+
+
+@pytest.mark.django_db
+def test_authlogout_get_request_from_authorized_user_successed(client, create_user, login_user):
+    url = reverse("logout")
+    response = client.get(url, follow=True)
+    user = auth.get_user(client)
+    assert response.status_code == 200
+    assertTemplateUsed(response, "home.html")
+    assert user.is_anonymous is True
+
+
+@pytest.mark.django_db
+def test_authlogout_get_request_from_unauthorized_user_redirect(client):
+    url = reverse("logout")
+    response = client.get(url, follow=True)
     assert response.status_code == 200
     assertTemplateUsed(response, "login.html")
