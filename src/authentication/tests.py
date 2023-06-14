278ffada3a42291d2b0f4e482d0159abc39eaa91
ProtKen <protcenko.kseniya@gmail.com>
2023-06-14 15:07:52 +0500

    tests: create tests for login page

--- src/authentication/tests.py
+++ src/authentication/tests.py
@@ -1,3 +1,55 @@
-from django.test import TestCase
+import pytest
+from django.contrib.auth.models import User
+from django.urls import reverse
+from pytest_django.asserts import assertTemplateUsed
 
-# Create your tests here.
+
+def test_authlogin_get_request_from_unauthorized_user_successed(client):
+    url = reverse("login")
+    response = client.get(url)
+    assert response.status_code == 200
+    assertTemplateUsed(response, "login.html")
+
+
+@pytest.mark.django_db
+def test_authlogin_get_request_from_authorized_user_redirect(client, create_user, login_user):
+    url = reverse("login")
+    response = client.get(url, follow=True)
+    assert response.status_code == 200
+    assertTemplateUsed(response, "home.html")
+
+
+@pytest.mark.django_db
+def test_authlogin_post_request_valid_user_data_successed(client, create_user):
+    url = reverse("login")
+    form_data = {
+        "username": "TestUser",
+        "password": "TestPassword",
+    }
+    response = client.post(url, data=form_data, follow=True)
+    assert response.status_code == 200
+    assertTemplateUsed(response, "user_profile.html")
+
+
+@pytest.mark.django_db
+def test_authlogin_post_request_not_valid_username_show_error_message(client, create_user):
+    url = reverse("login")
+    form_data = {
+        "username": "NotExistedUser",
+        "password": "TestPassword",
+    }
+    response = client.post(url, data=form_data)
+    assert response.status_code == 200
+    assertTemplateUsed(response, "login.html")
+
+
+@pytest.mark.django_db
+def test_authlogin_post_request_not_valid_password_show_error_message(client, create_user):
+    url = reverse("login")
+    form_data = {
+        "username": "TestUser",
+        "password": "WrongPassword",
+    }
+    response = client.post(url, data=form_data)
+    assert response.status_code == 200
+    assertTemplateUsed(response, "login.html")
