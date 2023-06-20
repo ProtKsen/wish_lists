340cb0f046e49e690f6cf340c954bc2eb84267bd
ProtKen <protcenko.kseniya@gmail.com>
2023-06-20 13:19:07 +0500

    tests: add fixture not active user

--- conftest.py
+++ conftest.py
@@ -17,3 +17,14 @@ def create_user(django_db_setup, django_db_blocker):
 def login_user(django_db_setup, django_db_blocker, client, create_user):
     client.login(username="TestUser", password="TestPassword")
     return client
+
+
+@pytest.fixture
+def create_not_active_user(django_db_setup, django_db_blocker):
+    with django_db_blocker.unblock():
+        user = User.objects.create_user(
+            username="NotActiveUser", email="test@test.com", password="TestPassword"
+        )
+        user.is_active = False
+        user.save()
+        HashSalt.objects.create(user=user, salt="TestSalt")
